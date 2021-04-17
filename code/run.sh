49cfd07ee6672ffac2890e55135a361ec786d5b1
Ariyan <44510322+ariyanzri@users.noreply.github.com>
2021-04-17 10:07:03 -0700

    Create run.sh

---
+++ code/run.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+mkdir drone_data
+cd drone_data
+
+wget https://de.cyverse.org/dl/d/7DB350A5-1EAB-4532-9C35-79AE08B9D9FF/2.27.20_P4_15m_RGB.tar.gz
+tar -xvf 2.27.20_P4_15m_RGB.tar.gz
+rm 2.27.20_P4_15m_RGB.tar.gz
+
