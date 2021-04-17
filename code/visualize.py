d20f43c88bf3336fa11109fb1234fc16c83a18b4
Ariyan <44510322+ariyanzri@users.noreply.github.com>
2021-04-17 11:29:57 -0700

    Update visualize.py

--- code/visualize.py
+++ code/visualize.py
@@ -11,16 +11,16 @@ images = []
 for f in list_files:
 #   image = mpimg.imread('{0}/{1}'.format('/home/ariyanzarei/drone_data/2.27.20_P4_15m_RGB/',f))
 #   images.append(image)
-  images.append(cv2.imread('{0}/{1}'.format('/home/ariyanzarei/drone_data/2.27.20_P4_15m_RGB/',f)))
+#   images.append(cv2.imread('{0}/{1}'.format('/home/ariyanzarei/drone_data/2.27.20_P4_15m_RGB/',f)))
 #   im = Image.open('{0}/{1}'.format('/home/ariyanzarei/drone_data/2.27.20_P4_15m_RGB/',f))
 #   images.append(im)
   break
   
   
-cv2.namedWindow('Image',cv2.WINDOW_NORMAL)
-cv2.resizeWindow('Image', 500,500)
-cv2.imshow('Image',images[0])
-cv2.waitKey(0)
+# cv2.namedWindow('Image',cv2.WINDOW_NORMAL)
+# cv2.resizeWindow('Image', 500,500)
+# cv2.imshow('Image',images[0])
+# cv2.waitKey(0)
       
 # plt.imshow(images[0])
 # plt.show()  
