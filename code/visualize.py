d6ef08d2c123ebb109f7b4bdb3d9f02daa1913da
Ariyan <44510322+ariyanzri@users.noreply.github.com>
2021-04-17 10:54:19 -0700

    Update visualize.py

--- code/visualize.py
+++ code/visualize.py
@@ -11,6 +11,8 @@ for f in list_files:
   image = mpimg.imread('{0}/{1}'.format('/home/ariyanzarei/drone_data/2.27.20_P4_15m_RGB/',f))
   images.append(image)
   
+  break
+  
 plt.imshow(images[0])
 plt.show()  
   
