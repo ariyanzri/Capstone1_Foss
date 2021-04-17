72881138d9911cdd9e289ca9b75fbce2cb5abca8
Ariyan <44510322+ariyanzri@users.noreply.github.com>
2021-04-17 11:21:06 -0700

    Update visualize.py

--- code/visualize.py
+++ code/visualize.py
@@ -1,29 +1,29 @@
-# import cv2
+import cv2
 import os
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-# from PIL import Image
+from PIL import Image
 
 list_files = os.listdir('/home/ariyanzarei/drone_data/2.27.20_P4_15m_RGB/')
 
 images = []
 
 for f in list_files:
-  image = mpimg.imread('{0}/{1}'.format('/home/ariyanzarei/drone_data/2.27.20_P4_15m_RGB/',f))
-  images.append(image)
-#   images.append(cv2.imread('{0}/{1}'.format('/home/ariyanzarei/drone_data/2.27.20_P4_15m_RGB/',f)))
+#   image = mpimg.imread('{0}/{1}'.format('/home/ariyanzarei/drone_data/2.27.20_P4_15m_RGB/',f))
+#   images.append(image)
+  images.append(cv2.imread('{0}/{1}'.format('/home/ariyanzarei/drone_data/2.27.20_P4_15m_RGB/',f)))
 #   im = Image.open('{0}/{1}'.format('/home/ariyanzarei/drone_data/2.27.20_P4_15m_RGB/',f))
 #   images.append(im)
   break
   
   
-# cv2.namedWindow('Image',cv2.WINDOW_NORMAL)
-# cv2.resizeWindow('Image', 500,500)
-# cv2.imshow('Image',images[0])
-# cv2.waitKey(0)
+cv2.namedWindow('Image',cv2.WINDOW_NORMAL)
+cv2.resizeWindow('Image', 500,500)
+cv2.imshow('Image',images[0])
+cv2.waitKey(0)
       
-plt.imshow(images[0])
-plt.show()  
+# plt.imshow(images[0])
+# plt.show()  
   
 # im.show()
   
