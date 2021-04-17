518e35fb6d3f2dd283e708634fdd4266aa77e366
Ariyan <44510322+ariyanzri@users.noreply.github.com>
2021-04-17 11:45:18 -0700

    Update visualize.py

--- code/visualize.py
+++ code/visualize.py
@@ -1,20 +1,24 @@
 import cv2
 import os
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
+# import matplotlib.pyplot as plt
+# import matplotlib.image as mpimg
 from PIL import Image
+import piexif
 
 list_files = os.listdir('/home/ariyanzarei/drone_data/2.27.20_P4_15m_RGB/')
 
 images = []
 
 for f in list_files:
+  img = Image.open('{0}/{1}'.format('/home/ariyanzarei/drone_data/2.27.20_P4_15m_RGB/',f))
+	exif_dict = piexif.load(img.info['exif'])
+  print(exif_dict)
 #   image = mpimg.imread('{0}/{1}'.format('/home/ariyanzarei/drone_data/2.27.20_P4_15m_RGB/',f))
 #   images.append(image)
 #   images.append(cv2.imread('{0}/{1}'.format('/home/ariyanzarei/drone_data/2.27.20_P4_15m_RGB/',f)))
 #   im = Image.open('{0}/{1}'.format('/home/ariyanzarei/drone_data/2.27.20_P4_15m_RGB/',f))
 #   images.append(im)
-  break
+#   break
   
   
 # cv2.namedWindow('Image',cv2.WINDOW_NORMAL)
