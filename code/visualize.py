0644506ef96bede0933a47a24e1820947ba63e6c
Ariyan <44510322+ariyanzri@users.noreply.github.com>
2021-04-17 10:41:03 -0700

    Update visualize.py

--- code/visualize.py
+++ code/visualize.py
@@ -1,4 +1,14 @@
 import cv2
 import os
 
-print(os.listdir('/home/ariyanzarei/drone_data/2.27.20_P4_15m_RGB/'))
+list_files = os.listdir('/home/ariyanzarei/drone_data/2.27.20_P4_15m_RGB/')
+
+cv2.namedWindow('Image',cv2.WINDOW_NORMAL)
+cv2.resizeWindow('Image', 500,500)
+
+for f in list_files:
+  image = cv2.imread('{0}/{1}'.format('/home/ariyanzarei/drone_data/2.27.20_P4_15m_RGB/',f))
+  cv2.imshow('Image',image)
+  r = cv2.waitKey(0)
+  print(r)
+  
