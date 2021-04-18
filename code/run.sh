ef77b8f84e0f8f61f589b57841666d19829240b2
W. Austin Rutherford <arutherford@email.arizona.edu>
2021-04-18 10:17:34 -0700

    Add annotations to script

--- code/run.sh
+++ code/run.sh
@@ -1,10 +1,20 @@
 #! /bin/bash
 
+# Go to existing directory 
 cd ~
+
+# Create a new directory called 'drone_data'
 mkdir drone_data
+
+# Change into directory 'drone_data'
 cd drone_data
 
+# Get data from CyVerse Data Store 
 wget https://de.cyverse.org/dl/d/7DB350A5-1EAB-4532-9C35-79AE08B9D9FF/2.27.20_P4_15m_RGB.tar.gz
+
+# Decompress tar file containing imagery file
 tar -xvf 2.27.20_P4_15m_RGB.tar.gz
+
+# remove the tar file (not needed anymore following decompressing)
 rm 2.27.20_P4_15m_RGB.tar.gz
 
